ba1b86603b3c00e2bd3c15a960b3fba9ddac156a
ivyz20 <86329150+ivyz20@users.noreply.github.com>
2025-11-06 16:59:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll(".fade-in");
+
+  const appearOptions = {
+    threshold: 0.2,
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+});
