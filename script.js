a932cc729589e4e5083ead4ea1b6d0c5f11b79c7
ivyz20 <86329150+ivyz20@users.noreply.github.com>
2025-11-06 17:25:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
     appearOnScroll.observe(fader);
   });
   
-});
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 
